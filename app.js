cea20ec62bf399ab18e6ea5b6ea49b1dd713c7b9
Kiran Anand <liteninkiran@hotmail.com>
2021-09-11 12:19:07 +0100

    Add routes

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/employees', (req, res) => {
+    res.send('Employees');
+});
+
 app.listen(3000);
 
+console.log('Listening on port 3000');
