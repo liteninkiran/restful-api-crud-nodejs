57c7a7b1ea669097e5bf378e882aa07222e5fe02
Kiran Anand <liteninkiran@hotmail.com>
2021-09-11 14:38:29 +0100

    Add employee

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const dotenv = require('dotenv');
 
 const connectDb = require('./config/db');
 
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,20 @@ router.get('/api/employees', (req, res) => {
     });
 });
 
+// Save record
+router.post('/api/employee/add', (req, res) => {
+    const emp = new Employee({
+        name: req.body.name,
+        email: req.body.email,
+        salary: req.body.salary,
+    });
+    emp.save((err, data) => {
+        res.status(200).json({
+            code: 200,
+            message: 'Employee added successfully',
+            addEmplyee: data,
+        });
+    });
+});
+
 module.exports = router;
